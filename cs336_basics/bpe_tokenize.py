1e9a9884b77e457e50e11822721ea9468dd7d1bb
yifan-lee <ivanlee142857@gmail.com>
2026-01-14 10:50:54 -0500

    replace token_freq with idx_freq to accelate, but not significant

--- cs336_basics/bpe_tokenize.py
+++ cs336_basics/bpe_tokenize.py
@@ -64,14 +64,15 @@ def find_chunk_boundaries(
     return sorted(set(chunk_boundaries))
 
 def update_token_freqs(
-    text_segment: str,
     token_freqs: dict[TOKEN, int],
+    text_segment: str,
 ):
     matches = re.finditer(PAT, text_segment)
     for m in matches:
         token = m.group()
         token_bytes = tuple(bytes([b]) for b in token.encode("utf-8"))
         token_freqs[token_bytes] += 1 
+
 
 def process_chunk(args):
     input_path, start, end, special_tokens, chunk_id = args
@@ -90,8 +91,36 @@ def process_chunk(args):
     token_freqs = defaultdict(int)
     # Use position=chunk_id+1 so 0 is left for the main bar
     for seg in segments:
-        update_token_freqs(seg, token_freqs)
+        update_token_freqs(token_freqs, seg)
     
+    return token_freqs
+
+def count_token_freqs_parallel(
+    input_path: str,
+    special_tokens: list[str],
+    num_processes: int = 4,
+):
+    with open(input_path, 'rb') as f:
+        split_token = special_tokens[0].encode("utf-8") if special_tokens else b""
+        boundaries = find_chunk_boundaries(f, num_processes, split_token) 
+    
+    token_freqs = defaultdict(int)
+
+    ## Initiate token_freqs
+    
+    tasks = []
+    for i in range(len(boundaries) - 1):
+        start = boundaries[i]
+        end = boundaries[i+1]
+        tasks.append((input_path, start, end, special_tokens, i))
+    
+    with ProcessPoolExecutor(max_workers=num_processes) as executor:
+        results = list(tqdm(executor.map(process_chunk, tasks), total=len(tasks), desc="Token Freqs (Chunks)", position=0))
+        
+        for local_freqs in results:
+            for token, count in local_freqs.items():
+                token_freqs[token] += count
+
     return token_freqs
 
 def get_pairs(
@@ -102,8 +131,8 @@ def get_pairs(
     return [(token[i], token[i+1]) for i in range(len(token)-1)]
 
 def update_pair_freqs(
-    token_freqs: dict[TOKEN, int],
     pair_freqs: dict[PAIR, int],
+    token_freqs: dict[TOKEN, int],
 ):
     for token, freq in token_freqs.items():
         if len(token) < 2:
@@ -112,26 +141,18 @@ def update_pair_freqs(
         for p in pairs:
             pair_freqs[p] += freq
 
-def update_pair2token(
+def update_pair2idx(
+    pair_to_idx: dict[PAIR, set[TOKEN]],
     token_freqs: dict[TOKEN, int],
-    pair_to_token: dict[PAIR, set[TOKEN]],
+    token_to_idx: dict[TOKEN, int],
 ):
     for token, freq in token_freqs.items():
         if len(token) < 2:
             continue
         pairs = get_pairs(token)
         for p in pairs:
-            pair_to_token[p].add(token)
+            pair_to_idx[p].add(token_to_idx[token])
 
-def update_token2pair(
-    token_freqs: dict[TOKEN, int],
-    token_to_pair: dict[TOKEN, list[PAIR]],
-):
-    for token, freq in token_freqs.items():
-        if len(token) < 2:
-            continue
-        pairs = get_pairs(token)
-        token_to_pair[token] = pairs
 
 def get_most_frequent_pair(
     pair_freqs: dict[PAIR, int],
@@ -139,40 +160,41 @@ def get_most_frequent_pair(
     return max(pair_freqs.keys(), key=lambda k: (pair_freqs[k], k))
 
 def update_vocab(
+    vocab: dict[int, bytes],
     new_id: int,
     best_pair: PAIR,
-    vocab: dict[int, bytes],
 ):
     new_vocab = best_pair[0] + best_pair[1]
     vocab[new_id] = new_vocab
 
 def update_vocab_inverse(
+    vocab_inverse: dict[bytes, int],
     new_id: int,
     best_pair: PAIR,
-    vocab_inverse: dict[bytes, int],
 ):
     new_vocab = best_pair[0] + best_pair[1]
     vocab_inverse[new_vocab] = new_id
 
 def update_merges(
-    best_pair: PAIR,
     merges: list[PAIR],
+    best_pair: PAIR,
 ):
     merges.append(best_pair)
 
 def update_all(
-    best_pair: PAIR,
-    pair_to_token: dict[PAIR, set[TOKEN]],
-    token_to_pair: dict[TOKEN, list[PAIR]],
-    token_freqs: dict[TOKEN, int],
+    pair_to_idx: dict[PAIR, set[int]],
     pair_freqs: dict[PAIR, int],
+    idx_freqs: dict[int, int],
+    idx_to_token: dict[int, TOKEN],
+    best_pair: PAIR,
 ):
-    affected_tokens = list(pair_to_token[best_pair])
+    affected_idxs = list(pair_to_idx[best_pair])
     merged_bytes = best_pair[0] + best_pair[1]
 
-    for token in affected_tokens:
+    for idx in affected_idxs:
         # get new token
         i=0
+        token = idx_to_token[idx]
         new_token = []
         while(i<(len(token))):
             if (i < len(token) - 1) and (token[i] == best_pair[0]) and (token[i+1] == best_pair[1]):
@@ -184,30 +206,28 @@ def update_all(
         new_token = tuple(new_token)
 
 
-        ## update pair_to_token
+        ## update pair_to_idx
         new_pairs = get_pairs(new_token)
-        affected_pairs = token_to_pair[token]
-        for pair in affected_pairs:
-            pair_to_token[pair].discard(token)
-        for pair in new_pairs:
-            pair_to_token[pair].add(new_token)
-
-
-        ## update token_to_pair
-        token_to_pair.pop(token)
-        token_to_pair[new_token] = new_pairs
+        affected_pairs = get_pairs(token)
+        for p in set(new_pairs)-set(affected_pairs):
+            pair_to_idx[p].add(idx)
+        for p in set(affected_pairs)-set(new_pairs):
+            pair_to_idx[p].discard(idx)
 
 
         ## update pair_freqs
         for pair in affected_pairs:
-            pair_freqs[pair] -= token_freqs[token]
+            pair_freqs[pair] -= idx_freqs[idx]
         for pair in new_pairs:
-            pair_freqs[pair] += token_freqs[token]
+            pair_freqs[pair] += idx_freqs[idx]
 
 
-        ## update token_freqs
-        origin_freq = token_freqs.pop(token)
-        token_freqs[new_token] = origin_freq
+        # ## update token_freqs
+        # origin_freq = token_freqs.pop(token)
+        # token_freqs[new_token] = origin_freq
+
+        ## update idx_to_token
+        idx_to_token[idx] = new_token
         
 
 def train_bpe(
@@ -231,54 +251,33 @@ def train_bpe(
     
     merges = []
 
-    print("Calculating chunk boundaries...")
-    t0 = time.time()
-    with open(input_path, 'rb') as f:
-        # Assuming first special token is the split token
-        split_token = special_tokens[0].encode("utf-8") if special_tokens else b"<|endoftext|>"
-        boundaries = find_chunk_boundaries(f, num_processes, split_token) # More chunks than processes for load balancing
-    print(f"Boundaries calculated in {time.time() - t0:.2f}s")
-    
-    token_freqs = defaultdict(int)
-
-    ## Initiate token_freqs
     print("Update Token Freq (Parallel)...")
     t0 = time.time()
-    
-    tasks = []
-    for i in range(len(boundaries) - 1):
-        start = boundaries[i]
-        end = boundaries[i+1]
-        tasks.append((input_path, start, end, special_tokens, i))
-    
-    with ProcessPoolExecutor(max_workers=num_processes) as executor:
-        results = list(tqdm(executor.map(process_chunk, tasks), total=len(tasks), desc="Token Freqs (Chunks)", position=0))
-        
-        for local_freqs in results:
-            for token, count in local_freqs.items():
-                token_freqs[token] += count
+
+    token_freqs = count_token_freqs_parallel(
+        input_path,
+        special_tokens,
+        num_processes,
+    )
                 
+    idx_to_token = {i: k for i, k in enumerate(token_freqs.keys())}
+    token_to_idx = {k: i for i, k in enumerate(token_freqs.keys())}
+    idx_freqs = {token_to_idx[token]: freq for token, freq in token_freqs.items()}
     print(f"Update Token Freq took {time.time() - t0:.2f}s")
 
-    pair_freqs = defaultdict(int)
-    pair_to_token = defaultdict(set)
-    token_to_pair = defaultdict(list)
 
     ## Initiate pair_freqs, pair_to_token, and token_to_pair
     print("Update Pair Freq...")
     t0 = time.time()
-    update_pair_freqs(token_freqs, pair_freqs)
+    pair_freqs = defaultdict(int)
+    update_pair_freqs(pair_freqs, token_freqs)
     print(f"Update Pair Freq took {time.time() - t0:.2f}s")
 
-    print("Update Pair to Token...")
+    print("Update Pair to idx...")
     t0 = time.time()
-    update_pair2token(token_freqs, pair_to_token)
+    pair_to_idx = defaultdict(set)
+    update_pair2idx(pair_to_idx, token_freqs, token_to_idx)
     print(f"Update Pair to Token took {time.time() - t0:.2f}s")
-
-    print("Update Token to Pair...")
-    t0 = time.time()
-    update_token2pair(token_freqs, token_to_pair)
-    print(f"Update Token to Pair took {time.time() - t0:.2f}s")
 
     print("Start Merging...")
     t0 = time.time()
@@ -292,15 +291,15 @@ def train_bpe(
         new_id = len(vocab)
 
         ## update vocab
-        update_vocab(new_id, best_pair, vocab)
+        update_vocab(vocab, new_id, best_pair)
 
         ## update vocab_inverse
-        update_vocab_inverse(new_id, best_pair, vocab_inverse)
+        update_vocab_inverse(vocab_inverse, new_id, best_pair)
 
         ## update merges
-        update_merges(best_pair, merges)
+        update_merges(merges, best_pair)
 
-        update_all(best_pair, pair_to_token, token_to_pair, token_freqs, pair_freqs)
+        update_all(pair_to_idx, pair_freqs, idx_freqs, idx_to_token, best_pair)
     print(f"Merging took {time.time() - t0:.2f}s")
     print(f"Total Training took {time.time() - start_total:.2f}s")
 
@@ -324,7 +323,7 @@ if __name__ == "__main__":
 
     vocab, merges = train_bpe(input_path, vocab_size, special_tokens,num_processes=16)
         
-    longest_values = sorted(vocab.values(), key=len, reverse=True)[:10]
+    longest_values = sorted(vocab.values(), key=len, reverse=True)[:30]
 
     for val in longest_values:
         print(f"Bytes: {val} | Size: {len(val)}")
